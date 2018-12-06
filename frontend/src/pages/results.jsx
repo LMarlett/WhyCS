4fea4cfd0957af2177e29c39c3d2b446c77f33c7
Lilli Jimenez <LilliMJimenez@gmail.com>
2018-12-06 16:02:57 -0500

    added social sharing

--- frontend/src/pages/results.jsx
+++ frontend/src/pages/results.jsx
@@ -5,6 +5,8 @@ import Footer from '../components/footerComponent/footer';
 import CategorySquare from '../components/categorySquare';
 import CategoryInformation from '../components/categoryInformation';
 import CategoryBlocksHeader from '../components/categoryBlocksHeader';
+import {NavLink, Button} from 'reactstrap';
+import  BaseButtonExample from '../components/socials';
 
 //to do get all results from db put into the state map thingy 
 //or a pie graph chart
@@ -21,8 +23,20 @@ import CategoryBlocksHeader from '../components/categoryBlocksHeader';
     this.setState({
       isOpen: !this.state.isOpen
     });
+
   }
+
+
+  componentWillUnmount() {
+    // fires immediately before component is unmounted
+    // from DOM (removed)
+    (window).unload(function(){
+      localStorage.clear();
+    });
+   }
+
   render() {
+    
     return (
          <div>
 
@@ -33,22 +47,46 @@ import CategoryBlocksHeader from '../components/categoryBlocksHeader';
           <div className="yourResultsWrapper">
             <div className="yourResultsInner">
               <h2>Your Results</h2>
-              {/* if else
-              
-              sort the key value pairs take the first three
-              
-              */}
+              <br />
+
               <CategorySquare />
- 
+             
+            <p className='text'>---- </p>
+
+
+            <div className="SocialsContainer">
+
+    <BaseButtonExample />
+   </div>
+
+            <a className="btn btn-danger text" role="button" href='cd front#impactAreas' >Learn more about impact areas</a>
+
+             
             </div>
           </div>
-
+              
           <div className="resultsChart">
+          <br />
           <p>Static content goes here</p>
+          <br />
+          <br />
+          <br />
+          <br />
+           <br />
+          <br /> 
+           <br />
+           <br />
+           <br />
+          <br />          <br />
+
+            <NavLink href="/resources" className="buttonWrapper">
+              <Button className="btn btn-danger text" role="button" >Do this with your colleagues</Button>
+              </NavLink>
+
           </div>
  
 
-          <div className="impactAreas">
+          <div className="impactAreas" id='impactAreas'>
             <h2>Learn More About Impact Areas</h2>
           {/* <NavLink href="#">
             <Button outline color="secondary">Learn More About Impact Areas</Button>
